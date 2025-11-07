578158e22dea1d3f7e6e90287e808c817a408c50
Mickey42302 <46900084+Mickey42302@users.noreply.github.com>
2025-11-06 21:51:32 -0500

    Add initial DevToolsInit class for mod setup

---
+++ src/main/java/com/mickey42302/devtools/DevToolsInit.java
@@ -0,0 +1,10 @@
+package com.mickey42302.devtools;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DevToolsInit.MODID)
+public final class DevToolsInit {
+
+    public static final String MODID = "mickey42302_devtools";
+
+}
